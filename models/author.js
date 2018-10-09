72045fa07063cc006e75b7f80fabc9c2c54a5ba4
Lab User <LabUser@labs.salina.k-state.edu>
2018-10-09 10:40:06 -0500

    Added delete pages and fixed lifespan of authors.

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -23,7 +24,7 @@ AuthorSchema
 .virtual('lifespan')
 .get(function () {
 	
-  return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+  return ((this.date_of_birth ? moment(this.date_of_birth).format('MMMM Do, YYYY') : '') + " - " + (this.date_of_death ? moment(this.date_of_death).format('MMMM Do, YYYY') : ''));
 });
 
 // Virtual for author's URL
